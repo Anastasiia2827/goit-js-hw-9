4e1d4eb0b168608545467beaab1f57792fd718a0
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-01-14 23:16:20 +0200

    1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-console.log('привіт')
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function color() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(color, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+})
+  

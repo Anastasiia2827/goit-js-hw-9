b340c211623d75d955f758cb59e2fdb51f8337be
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-01-16 14:05:06 +0200

    03task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from "notiflix";
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  });
 }
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { amount, delay, step } = e.target.elements;
+  let valueDelay = +delay.value;
+  for (let i = 1; i <= amount.value; i += 1) {
+      createPromise(i,valueDelay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delayValue += +step.value
+  }
+  e.target.reset()
+}
+
+  
+  
+  
+  
+  
+  
+  
+
+
